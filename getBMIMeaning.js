44384110d2eafe5bff8b11c49f8a8c0aec006b61
bamxxls <thaksaporn.xxls@mail.kmutt.ac.th>
2022-09-09 16:44:20 +0700

    DONEgit status

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,19 +1,22 @@
 const { template } = require('@babel/core')
 
+
 function calculateBMI(weight, height) {
-  let BMI = weight/(height*height);
+  let BMI = weight/(height*height)
   return BMI;
 }
-function getBMIMeaning(weight, height) {
-  if(calculateBMI() < 18.5){
-  return "Underweight";
+
+function getBMIMeaning(weight, height){
+  let BMI=calculateBMI(weight, height)
+  if(BMI < 18.5){
+  return 'Underweight';
   }
-  if(calculateBMI() >= 18.5 && calculateBMI() <= 24.9){
-    return "Normal Weight";
+  if(BMI <= 24.9){
+    return 'Normal weight';
   }
-  if(calculateBMI() > 25){
-    return "Overweight";
+  if(BMI > 25){
+    return 'Overweight';
   }
 }
-module.exports = getBMIMeaning
 
+module.exports = getBMIMeaning
